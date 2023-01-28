1cf31281eabaf1baab236a8d5b1e959fa0cc72a4
z20axa <alejandrospan@hotmail.com>
2023-01-27 20:59:32 -0500

    Adds the user model js file

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const {Schema,model} = require('mongoose');
+
+const userSchema = new Schema({
+    username: {type: String, unique: true, required: true, , trim: true}, 
+    email: {type: String, required: true, unique: true, match: /^([A-Za-z0-9_\.-]+)@([\dA-Za-z\.-]+)\.([A-Za-z\.]{2,6})$/ }, 
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Thought"
+        },
+
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+    ],
+},
+
+{
+    toJSON: {
+        virtuals: true,
+    },
+        id: false,
+},
+
+);
+
+userSchema.virtual("friendCount").get(function(){
+    return this.friends.length
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
