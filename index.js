40a845f07a7f21eec3edc296187fdc0d5b09acfd
z20axa <alejandrospan@hotmail.com>
2023-02-04 18:19:32 -0500

    Adds comments to the index js server file

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
+// packages/modules imports
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+//
 const cwd = process.cwd();
 
-const PORT = process.env.PORT || 3001;
+// initialize our app variable by setting it to the value of express()
 const app = express();
+
+// setting computer port number for service and Heruko deployment
+const PORT = process.env.PORT || 3001;
 
 // Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
 const activity = cwd.includes('01-Activities')
   ? cwd.split('/01-Activities/')[1]
   : cwd;
 
-app.use(express.urlencoded({ extended: true }));
+// body parsing
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes 
 app.use(routes);
 
+// keep your web service awake and listening
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server for ${activity} running on http://localhost:${PORT}`);
