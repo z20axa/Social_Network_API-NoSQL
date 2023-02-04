760f8d7d01cd9629aa46c1897e0a0e890abf3ccc
z20axa <alejandrospan@hotmail.com>
2023-02-04 18:51:22 -0500

    Revises index API route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+const usersRoutes = require('./usersRoutes');
 const thoughtsRoutes = require('./thoughtsRoutes');
 
 router.use('/users', userRoutes);
