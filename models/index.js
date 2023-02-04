980825f4a986f71689e522dc4e0e1bcc115b84d2
z20axa <alejandrospan@hotmail.com>
2023-02-04 18:13:21 -0500

    Adds revisiosn to the model files

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
-const Student = require('./Student');
-const Course = require('./Course');
+// module imports
 const User = require("./Student");
 const Thought = require("./Thought");
 
-module.exports = { Student, Course, User, Thought };
+module.exports = { User, Thought };
