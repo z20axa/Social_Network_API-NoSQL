58a6b05fdd354c03d8bc8c182e3430c1344c4304
z20axa <alejandrospan@hotmail.com>
2023-02-04 19:23:25 -0500

    Adds comments to the api index route file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,12 @@
+// pacakges and module imports
 const router = require('express').Router();
 const usersRoutes = require('./usersRoutes');
 const thoughtsRoutes = require('./thoughtsRoutes');
 
+// api/users
 router.use('/users', userRoutes);
+
+// api/thoughts
 router.use('/thoughts', thoughtsRoutes);
 
 module.exports = router;
